793cee32b2cf4355e1ef8a798595682d5006526a
Michael van Tulder <michaelvtulder@gmail.com>
2023-06-11 17:12:11 +0200

    addData test

--- Assignment/addData.py
+++ Assignment/addData.py
@@ -1,6 +1,10 @@
-import sys
-sys.path.append('../Python3Code')
-from Python3Code.Chapter2.CreateDataset import CreateDataset
+import sys, os
+
+sys.path.append("Python3Code")
+from Chapter2.CreateDataset import CreateDataset
+from util.VisualizeDataset import VisualizeDataset
+from util import util
+
 from pathlib import Path
 import os
 
@@ -23,8 +27,16 @@ folders = get_folders_in_path(DATASET_PATH)
 for folder in folders:
     dataset.add_numerical_dataset(folder + '/Barometer.csv', 'Time (s)', ['X (hPa)'], 'avg', 'barometer_')
     dataset.add_numerical_dataset(folder + '/Gyroscope.csv', 'Time (s)', ['X (rad/s)', 'Y (rad/s)', 'Z (rad/s)'], 'avg', 'gyroscope_')
-    dataset.add_numerical_dataset(folder + '/Accelerometer.csv', 'Time (s)', ['X (m/s^2)', 'Y (m/s^2)', 'Z (m/s^2)'], 'avg', 'accelerometer_')
+    dataset.add_numerical_dataset(folder + '/Linear Accelerometer.csv', 'Time (s)', ['X (m/s^2)', 'Y (m/s^2)', 'Z (m/s^2)'], 'avg', 'accelerometer_')
     dataset.add_numerical_dataset(folder + '/Location.csv', 'Time (s)', ['Velocity (m/s)'], 'avg', 'location_')
     dataset.add_event_dataset(folder + '/labels.csv', 'label_start', 'label_end', 'label', 'binary')
 
+dataset = dataset.data_table
+util.print_statistics(dataset)
+# Plot the data
+DataViz = VisualizeDataset(__file__)
+#DataViz.plot_dataset_boxplot(dataset, ['gyroscope_X (rad/s)','gyroscope_Y (rad/s) ','gyroscope_Z (rad/s) ','accelerometer_X (m/s^2)','accelerometer_Y (m/s^2)','accelerometer_Z (m/s^2)'])
+DataViz.plot_dataset_boxplot(dataset, ['accelerometer_X (m/s^2)', 'accelerometer_Y (m/s^2)', 'accelerometer_Z (m/s^2)', 'gyroscope_X (rad/s)', 'gyroscope_Y (rad/s)', 'gyroscope_Z (rad/s)'])
+
+# Plot all data
 dataset.to_csv(RESULT_PATH / RESULT_FNAME)
